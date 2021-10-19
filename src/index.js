0a07798076e5a8fb485cf1ed0016cf985b0d606f
4slash271 <4slash271@gmail.com>
2021-10-19 17:07:04 +0900

    2.0 create components in react using jsx

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Test from './test';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /><Test/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/test.js
@@ -0,0 +1,8 @@
+import React from "react";
+function Test(){
+    return(
+        <h2>It is a text page!</h2>
+    )
+}
+
+export default Test;
