16f9e4e0d2acc7614930ff81eafd2149bc02bfd3
4slash271 <4slash271@gmail.com>
2021-10-20 11:05:59 +0900

    2.1 Reusable Components with JSX and Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+function Food({menu}){
+  return (
+    <h1>I like {menu}</h1>
+  )
+}
 
 function App() {
   return (
-    <div>Hello!!</div>
+    <div>Hello!!
+      <Food menu="kimchi"/>
+      <Food menu="bibimbap"/>
+      <Food menu="gimbap"/>
+      <Food menu="pizza"/>
+      <Food menu="ramen"/>
+      <Food menu="jajangmyeon"/>
+      <Food menu="chicken"/>
+    </div>
+   
   );
 }
 
--- src/test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-function Test(){
-    return(
-        <h2>It is a text page!</h2>
-    )
-}
-
-export default Test;
