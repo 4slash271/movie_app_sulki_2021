0a5af115b9ed207aad3fdeee2682a00b11ce92dd
4slash271 <4slash271@gmail.com>
2021-10-21 18:11:16 +0900

    2.2 map

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from 'react';
 
-function Food({menu}){
-  return (
-    <h1>I like {menu}</h1>
-  )
-}
-function Animal({pets}){
-  return<h2>I like also a {pets}</h2>
+function Animal({name, pics}){
+  return(
+  <div>
+    <h1>I like a {name}!</h1>
+    <img src={pics}/>
+  </div>
+  );
 }
 
+const animalLike = [
+  {
+    name: "cat",
+    image: "https://i.kym-cdn.com/entries/icons/original/000/026/489/crying.jpg" 
+  },
+  {
+    name: "rabbit",
+    image: "https://cuzrabbits.files.wordpress.com/2021/03/polish_20191130_222814796.jpg"
+  },
+  {
+    name: "dog",
+    image: "https://www.memeandfacts.com/wp-content/uploads/2021/03/dog-meme-template-8-780x780.jpg"
+  },
+  {
+    name: "rat",
+    image: "https://i.chzbgr.com/full/8241272832/h3CA500E0/like-the-mouse-version-of-sam-fischer"
+  },
+  {
+    name: "bear",
+    image: "https://i.imgflip.com/222bjf.jpg"
+  },
+  {
+    name: "bird",
+    image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/67CF/production/_108857562_mediaitem108857561.jpg"
+  }
+];
 function App() {
   return (
-    <div>Hello!!
-      <Food menu="kimchi"/>
-      <Food menu="bibimbap"/>
-      <Food menu="gimbap"/>
-      <Food menu="pizza"/>
-      <Food menu="ramen"/>
-      <Food menu="jajangmyeon"/>
-      <Food menu="chicken"/>
-      <Animal pets = "cat"/>
-      <Animal pets = "rabbit"/>
-      <Animal pets = "dog"/>
-      <Animal pets = "rat"/>
-      <Animal pets = "bear"/>
-      <Animal pets = "bird"/>
+    <div>
+     {animalLike.map(pets => (
+     <Animal name = {pets.name} pics = {pets.image}/>
+     ))}
     </div>
    
   );
