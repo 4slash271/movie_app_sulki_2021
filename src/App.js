dfa1551cd58554cb9ee2b3627d19537b748cd53a
4slash271 <4slash271@gmail.com>
2021-11-08 23:56:48 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
-import React from "react";
-import axios from "axios";
-import Movie from "./Movies";
-import "./App.css";
 
-class App extends React.Component {
-  state = {
-    isLoading : true,
-    movies: [],
-  };
-  getMovies = async() =>{
-   const {
-     data: {
-       data:{movies}
-      }
-     } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
-     this.setState({movies, isLoading : false});
-  }
-  componentDidMount(){
-    this.getMovies();
-  }
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-    <section class= "container">
-      {isLoading ? (
-      <div class= "loader">
-        <span class = "loader__text">Loading...</span>
-      </div>
-      ) : (<div class ="movies">
-        {movies.map(movie =>(
-        <Movie 
-        key = {movie.id}
-          id = {movie.id}
-          year = {movie.year}
-          title = {movie.title}
-          summary = {movie.summary}
-          poster = {movie.medium_cover_image}
-          genres = {movie.genres}
-          />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" ><h1>HOME</h1></Route>
+      <Route path="/about"><h1>ABOUT</h1></Route>
+    </HashRouter>
+  );
 }
+
 export default App;
