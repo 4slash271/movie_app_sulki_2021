6707b38525a21a5d796613611897c3ea29c98198
4slash271 <4slash271@gmail.com>
2021-11-14 23:46:29 +0900

    ...

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,36 @@
-import React from "react";
 
-class Detail extends React.Component {
-  componentDidMount() {
-    const { location, history } = this.props;
-    if (location.state === undefined) {
-      history.push("/");
-    }
-  }
-  render() {
-    const { location } = this.props;
-    if (location.state) {
-      return <span>{location.state.title}</span>;
+import React from "react";
+function Detail(Props){
+    console.log(Props);return<span>Hello</span>;
+
+}
+// class Detail extends React.Component {
+//   componentDidMount() {
+//     const { location, history } = this.props;
+//     if (location.state === undefined) {
+//       history.push("/");
+//     }
+//   }
+//   render() {
+//     const { location } = this.props;
+//     if (location.state) {
+//       return <span>{location.state.title}</span>;
+//     } else {
+//       return null;
+//     }
+//   }
+// }
+render() {
+    let location;
+    if(check if props has val) {
+        location = <MyComponent />
     } else {
-      return null;
+        location = null
     }
-  }
+    return (
+        <div>
+            {location}
+        </div>
+    )
 }
 export default Detail;
